979e39cf02bad2767687a2c6ed3f101b49dc1bbe
RANADE GAURI DNYANESHWAR <gauriranade908@gmail.com>
2025-01-08 08:14:26 +0530

    Update 9-sqrtx.java

--- 9-sqrtx.java
+++ 9-sqrtx.java
@@ -3,14 +3,14 @@ class Solution {
     {
        long start=0;
        long end=x;
-       while(start+1<end)
-       {
-        long mid=start+(end-start)/2;
-        if (mid*mid==x)
-        {
-            return (int)mid;
-        }
-        else if(mid*mid<x)
+             while(start+1<end)
+                  {
+                     long mid=start+(end-start)/2;
+                     if (mid*mid==x)
+              {
+                     return (int)mid;
+                }
+                   else if(mid*mid<x)
         {
             start=mid;
         }
